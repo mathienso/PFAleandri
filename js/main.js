de53cde6fcc1fab5780b9150c5fdc6a6fc0f5cba
Mathias Aleandri <mathias.aleandri@gmail.com>
2023-04-16 16:36:35 -0300

    PreEntrega2Aleandri

--- js/main.js
+++ js/main.js
@@ -1,36 +1,67 @@
 /* 
-    Primer entrega:
-    Usar lo visto en clase (ej. funciones de tipos de datos en js, no hay objetos).
-    Usar variables, funciones, prompt y alert. Tambien usar ciclos y condicionales.
-    Simulador interactivo: la pagina tiene que pedir cosas al entrar.
-    Agregar readme que indique como debe ser usado el programa.
-
-    Funcionalidad pre entrega 1 -> Comprar sneakers:
+    Funcionalidad pre entrega 2 -> Comprar sneakers:
     1. Crear un programa que pida al usuario ingresar nombre y valor de un producto (zapatillas en mi caso) la cantidad de veces que quiera.
     2. Se debe dejar vacío el nombre del producto para finalizar la compra.
-    3. Mostrar precio final y cantidad de productos (la lista de productos no se puede porque no vimos arrays todavía).
+    3. Mostrar precio final, cantidad y lista de productos.
 */
 
-let total = 0;
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = parseFloat(price);
+    }
+}
+
+class Cart {
+    constructor(id) {
+        this.id = id;
+        this.products = [];
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    getTotal() {
+        let total = 0;
+        this.products.forEach(product => {
+            total += product.price;
+        });
+        return total;
+    }
+
+    printSneakers() {
+        let text = "";
+        this.products.forEach(sneaker => {
+            text += ` ${sneaker.name}: $${sneaker.price}\n`;
+        });
+        return text;
+    }
+
+    printTicket(date) {
+        alert(
+            `Felicidades por su compra del ${date.toLocaleDateString()}.\n${this.printSneakers()}\nUsted compró ${this.products.length} sneakers por un total de $${this.getTotal()}.`
+        );
+    }
+
+    isEmpty() {
+        return this.products.length == 0;
+    }
+}
+let cart = new Cart(1);
+
 let sneaker;
-let price;
-let sneakersCounter = 0;
-
-function addCart(price) {
-  total += parseFloat(price);
-}
-
+let price = 0;
 while (sneaker != "") {
-  sneaker = prompt(
-    "Ingrese el modelo de su Sneaker (enviar en blanco para no cargar más Sneakers)"
-  );
-  if (sneaker != "") {
-    sneakersCounter++;
-    price = parseFloat(prompt("Ingrese el valor de su Sneaker"));
-    addCart(price);
-  }
+    sneaker = prompt(
+        "Ingrese el modelo de su Sneaker (enviar en blanco para no cargar más Sneakers)"
+    );
+    if (sneaker != "") {
+        price = parseFloat(prompt("Ingrese el valor de su Sneaker"));
+        cart.addProduct(new Product(sneaker, price));
+    }
 }
 
-alert(
-  "Usted compró " + sneakersCounter + " Sneakers por un total de $" + total
-);
+if(!cart.isEmpty()){
+    cart.printTicket(new Date());
+}
--- readme.md
+++ readme.md
@@ -10,11 +10,16 @@ Curso de Javascript - Proyecto final: SneakyDrops
 El presente contiene algunas indicaciones sobre el contenido del proyecto usado como ejemplo.
 
 Como ser los siguientes:
-* Estructura de control WHILE.
+* Estructura de control FOR y WHILE.
 * Estructura de control IF.
 * Formas de interactuar con el usuario, con prompt y alert.
+* Clases y Objetos.
+* Arrays.
 
-Funcionalidad pre entrega 1 -> Comprar sneakers:
+Funcionalidad pre entrega 2 -> Comprar sneakers:
 1. Crear un programa que pida al usuario ingresar nombre y valor de un producto (zapatillas en mi caso) la cantidad de veces que quiera.
 2. Se debe dejar vacío el nombre del producto para finalizar la compra.
-3. Mostrar precio final y cantidad de productos (la lista de productos no se puede porque no vimos arrays todavía).
+3. Mostrar precio final, cantidad y lista de productos.
+
+## Notas
+No se añaden validaciones de nombre y precio ya que en las primeras entregas los ingresa el usuario y en el proyecto final esto no sucederá.
